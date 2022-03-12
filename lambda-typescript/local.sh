315874b96d1c8aa17db1f40858ede5cf188db01c
Marco Gatti <marco.gatti@cubbit.io>
2022-03-12 18:02:59 +0100

    feat: add local invoke test
    * add event example for lambda-nestjs

--- lambda-typescript/local.sh
+++ lambda-typescript/local.sh
@@ -2,6 +2,8 @@
 
 SAM_CLI_TELEMETRY=0
 
+sam local invoke -e events/event.json
+
 sam build --beta-features
 
 sam local start-api
--- sam-hello-native/local.sh
+++ sam-hello-native/local.sh
@@ -2,6 +2,8 @@
 
 SAM_CLI_TELEMETRY=0
 
+sam local invoke -e events/event.json
+
 sam build --use-container
 
 sam local start-api
